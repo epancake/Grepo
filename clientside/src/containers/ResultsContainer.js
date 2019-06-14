29bdde2c732156521b1b873a001d311c1e2f18de
Emily Pancake <epancake24@gmail.com>
2019-06-13 19:35:37 -0600

    add screenshot

--- clientside/src/containers/ResultsContainer.js
+++ clientside/src/containers/ResultsContainer.js
@@ -6,7 +6,7 @@ import Result from '../components/Result'
 const ResultsContainer = ({results}) => {
   if (results.result) {
     return (
-      <div>Results:
+      <div className='results-list'>Results:
         {results.result.items.map(result => (
             <Result
               key={result.id}
